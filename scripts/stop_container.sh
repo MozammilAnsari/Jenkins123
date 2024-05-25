4155e98a47023d1568f576b5cbc6866114c38be7
MozammilAnsari <93940544+MozammilAnsari@users.noreply.github.com>
2024-05-26 00:40:20 +0530

    Add files via upload

---
+++ scripts/stop_container.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Stop the running container (if any)
+echo "Stopping container"
+
+containerId=`sudo docker ps | awk -F " " '{print $1}'`
+
+sudo docker rm -f $containerId
+
+
