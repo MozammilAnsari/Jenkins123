a4492cf0ecbdbaf4ab4306d0e7ba5c730266d31c
MozammilAnsari <93940544+MozammilAnsari@users.noreply.github.com>
2024-05-26 00:41:56 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,11 +3,11 @@ set -e
 
 echo "Pulling image from docker"
 
-docker pull modassir7488/my-app:tagname
+docker pull modassir7488/demo-jenkins:1
 
 
 # Run the Docker image as a container
 echo "Run the docker image"
 
-docker run -d -p 5000:5000 modassir7488/my-app:tagname
+docker run -d -p 80:80 modassir7488/demo-jenkins:1
 
